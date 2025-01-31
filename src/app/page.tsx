e657cb79b4dae50efdb0ba3138233fba4b063c67
Wayne Worth <dubdotworth@gmail.com>
2025-01-30 23:15:37 -0600

    remove txntable from demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import { TxnTable } from '@/components/TxnTable';
 import { TxnTableWithFilter } from '@/components/TxnTableWithFilter';
 
 export default async function Page() {
 	return (
 		<div className='my-20 flex flex-col gap-8'>
-			<TxnTable />
 			<TxnTableWithFilter />
 		</div>
 	);
